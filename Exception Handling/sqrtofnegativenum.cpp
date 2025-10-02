3e6b8d3a339c8cbefd5df806cf96107e4d9bac66
sashipuri <sashipuri@ioepc.edu.np>
2025-10-02 11:21:45 +0545

    exception

---
+++ Exception Handling/sqrtofnegativenum.cpp
@@ -0,0 +1,34 @@
+#include<iostream>
+#include<math.h>
+using namespace std;
+ int main() 
+{
+    int num;
+    cout<<"Enter th value of a number"<<endl;
+    cin>>num;
+
+    try
+    {
+        if(num<0)
+        {
+            throw(num);
+        }
+        else
+        {
+            cout<<"The Square root of given no is"<<sqrt(num)<<endl;
+        }
+    }
+    catch(int i)
+
+    {
+        //cout<<"Squareroot of -ve number "<<n1<<"cannot be found"<<endl;
+        float n2 = fabs(i);
+        cout<<"The squareroot is "<<sqrt(n2)<<endl;
+      
+    }
+    
+  
+  
+  
+return 0;
+}
